90bace46fb91b42d3d0d17367c16db81c0187cdb
Saksham8g <144920519+Saksham8g@users.noreply.github.com>
2024-05-22 19:35:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is cInitialized!');
+}
+
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+        {
+            console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            console.log("noseX = " + noseX +" noseY = " + noseY);
+
+
+            
+        }
+}
